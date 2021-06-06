92687786e3287bea1ed90ce60d6b427a6cfe7617
darkload-jk <80630822+darkload-jk@users.noreply.github.com>
2021-06-06 22:26:07 +0900

    Add files via upload

--- confirmation.php
+++ confirmation.php
@@ -7,13 +7,20 @@
     <title>Reservation</title>
 </head>
 <body>
-    <h1>Reservation Completed!!</h1>
-    <h2>If you finish, click <a value=logout href="login.php">here</a> to logout.</h2>
-    <h2>IF you change your order, click <a href="dashboard.php">here</a> to back to the order page.</h2>
-    <?php
-        if(isset($_POST['logout'])) {
-            mysqli_close($conn);
-        }
-    ?>
+<div class="container-fluid mt-3">
+        <div class="jumbtron bg-info text-right">
+            <h1 class="display-4 text-center">Reservation Completed!!</h1>
+        </div>
+        <div class="card mx-auto w-50 my-5 border-0">
+            <h2>If you finish, click <a value=logout href="login.php">here</a> to logout.</h2>
+            <h2>If you change your order, click <a href="dashboard.php">here</a> to back to the order page.</h2>
+            <h2>If you check your order, click <a href="profile.php">here</a> and check whether you pay.</h2>
+            <?php
+                if(isset($_POST['logout'])) {
+                    mysqli_close($conn);
+                }
+            ?>
+        </div>
+</div>
 </body>
 </html>
